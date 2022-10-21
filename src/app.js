5284ebf7e07dcf5ce958d8a68e5821d1bd55d7ae
PatriciaMG92 <xiqitinah_92@hotmail.com>
2022-10-21 10:14:29 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excusa = document.getElementById("excuse");
+  excusa.innerHTML = generarExcusa();
 };
+function generarExcusa() {
+  let quien = ["Mi perro", "Mi cuñado", "La gata", "El vecino"];
+  let accion = ["se ha comido", "ha tirado", "ha roto", "ha quemado"];
+  let que = ["mis deberes", "la televisión", "la moto", "el ordenador"];
+  let cuando = [
+    "esta misma mañana.",
+    "durante la comida.",
+    "cuando estaba a punto de salir de casa.",
+    "por la noche."
+  ];
+
+  let indiceQuien = Math.floor(Math.random() * quien.length);
+  let indiceAccion = Math.floor(Math.random() * accion.length);
+  let indiceQue = Math.floor(Math.random() * que.length);
+  let indiceCuando = Math.floor(Math.random() * cuando.length);
+
+  return (
+    quien[indiceQuien] +
+    " " +
+    accion[indiceAccion] +
+    " " +
+    que[indiceQue] +
+    " " +
+    cuando[indiceCuando]
+  );
+}
